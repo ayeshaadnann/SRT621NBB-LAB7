8ec2be04f4707a0e0c3ae05d7395c251dfa60d99
ayeshaadnann <ayesha.adnan1999@gmail.com>
2022-03-15 23:39:11 -0400

    chnages 4

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var http = require('http');
-var url = require('url');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-    var q = url.parse(req.url, true).query;
+const port = 8989,
+http = require("http"),
+httpStatus = require("http-status-codes"),
+app = http.createServer((request, response) => {
+console.log("Received an incoming request!");
+response.writeHead(httpStatus.OK,
+{"Content-Type":"text/html"});
     var today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    var dateTime = date+' '+time;   
-    var txt = dateTime;
-  res.end(txt);
-}).listen(8989);
+    var dateTime = date+' '+time; 
+let responseMessage= dateTime;
+response.write(responseMessage);
+response. end();
+console.log( `Sent a response : ${responseMessage}`);
+ });
+app.listen(port);
+console.log(`The server has started and is listenign on port number: ${port}`);;

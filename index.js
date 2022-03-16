b21121f3d1b9fc249eea9e521b47b4f4ddc45ef8
ayeshaadnann <ayesha.adnan1999@gmail.com>
2022-03-15 21:48:56 -0400

    second step

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log()
-console.log()
+var http = require('http');
+var url = require('url');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+    var q = url.parse(req.url, true).query;
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;   
+    var txt = dateTime;
+  res.end(txt);
+}).listen(8989);
